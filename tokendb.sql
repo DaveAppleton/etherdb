7b7b4483b50914318fd92395d200ab7793812421
Dave Appleton <calistralabs@gmail.com>
2018-04-04 18:23:16 +0800

    including timestamp in sql

--- tokendb.sql
+++ tokendb.sql
@@ -22,8 +22,27 @@ create table tokentransfers (
 	txhash varchar(70) unique not null,
     source varchar(44) not null,
     dest varchar(44) not null,
+    "timestamp" integer,
     Amount varchar(44) not null
 );
+
+-- DROP TABLE public.tokentransfers;
+
+CREATE TABLE tokentransfers
+(
+    transferid integer NOT NULL DEFAULT nextval('tokentransfers_transferid_seq'::regclass),
+    tokenid integer NOT NULL,
+    blocknumber integer,
+    blockhash character varying(70) COLLATE pg_catalog."default" NOT NULL,
+    index integer,
+    txhash character varying(70) COLLATE pg_catalog."default" NOT NULL,
+    source character varying(44) COLLATE pg_catalog."default" NOT NULL,
+    dest character varying(44) COLLATE pg_catalog."default" NOT NULL,
+    amount character varying(44) COLLATE pg_catalog."default" NOT NULL,
+    "timestamp" integer,
+    CONSTRAINT tokentransfers_pkey PRIMARY KEY (transferid),
+    CONSTRAINT dup_txhash UNIQUE (txhash)
+)
 
 grant select, insert, update on tokentransfers to token;
 grant select,  update on tokentransfers_transferid_seq to token;
