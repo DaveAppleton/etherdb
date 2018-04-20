bea75f25d2fac35095bd8a65153a4ad278aebaf5
Dave Appleton <calistralabs@gmail.com>
2018-04-21 03:06:05 +0800

    NamedTokenTransfers

--- tokentransferdb.go
+++ tokentransferdb.go
@@ -20,6 +20,23 @@ type TokenTransfer struct {
 	Amount      string
 }
 
+type NamedTokenTransfer struct {
+	TransferID    uint64
+	TokenID       uint64
+	TokenName     string
+	TokenSymbol   string
+	TokenDecimals uint8
+	TokenAddress  string
+	BlockNumber   uint64
+	BlockHash     string
+	Timestamp     uint64
+	Index         uint
+	TxHash        string
+	Source        string
+	Dest          string
+	Amount        string
+}
+
 // Add this token to the database
 func (tt *TokenTransfer) Add() (err error) {
 	statement := `insert into tokentransfers (tokenid,blocknumber,blockhash,index,txhash,source,dest,amount,timestamp) values ($1,$2,$3,$4,$5,$6,$7,$8,$9) returning transferid`
@@ -57,6 +74,18 @@ func getTransfers(rows *sql.Rows) (transfers []TokenTransfer, err error) {
 	return
 }
 
+func getNamedTransfers(rows *sql.Rows) (transfers []NamedTokenTransfer, err error) {
+	var t NamedTokenTransfer
+	for rows.Next() {
+		err = rows.Scan(&t.TransferID, &t.TokenID, &t.BlockNumber, &t.BlockHash, &t.Index, &t.TxHash, &t.Source, &t.Dest, &t.Amount, &t.Timestamp, &t.TokenName, &t.TokenSymbol, &t.TokenDecimals, &t.TokenAddress)
+		if err != nil {
+			return
+		}
+		transfers = append(transfers, t)
+	}
+	return
+}
+
 // Find transfers that match tokenID
 func (tt *TokenTransfer) Find() (transfers []TokenTransfer, err error) {
 	statement := `select transferid,tokenid,blocknumber,blockhash,index,txhash,source,dest,amount,timestamp from tokentransfers where tokenid=$1`
@@ -69,15 +98,7 @@ func (tt *TokenTransfer) Find() (transfers []TokenTransfer, err error) {
 		return
 	}
 	return getTransfers(rows)
-	// var t TokenTransfer
-	// for rows.Next() {
-	// 	err = rows.Scan(&t.TransferID, &t.TokenID, &t.BlockNumber, &t.BlockHash, &t.Index, &t.TxHash, &t.Source, &t.Dest, &t.Amount, &t.Timestamp)
-	// 	if err != nil {
-	// 		return
-	// 	}
-	// 	transfers = append(transfers, t)
-	// }
-	// return
+
 }
 
 // FindByAddress returns transfers of specific token to or from an address
@@ -94,9 +115,13 @@ func (tt *TokenTransfer) FindByAddress(addr string) (transfers []TokenTransfer, 
 	return getTransfers(rows)
 }
 
-// FindAllByAddress returns transfers of any token to or from an address
-func (tt *TokenTransfer) FindAllByAddress(addr string) (transfers []TokenTransfer, err error) {
-	statement := `select transferid,tokenid,blocknumber,blockhash,index,txhash,source,dest,amount,timestamp from tokentransfers where source=$1 or dest=$1`
+// FindAllByAddress returns transfers of any token to or from an address newest first
+func (tt *NamedTokenTransfer) FindAllByAddress(addr string) (transfers []NamedTokenTransfer, err error) {
+	statement := `select transferid,A.tokenid,blocknumber,blockhash,index,txhash,source,dest,amount,timestamp,name,symbol,decimals,address
+		from tokentransfers A, tokens B
+		 where A.tokenid = B.tkn
+		 and (source=$1 or dest=$1)
+		 order by blocknumber desc, index desc`
 	stmt, err := db.Prepare(statement)
 	if err != nil {
 		return
@@ -105,7 +130,7 @@ func (tt *TokenTransfer) FindAllByAddress(addr string) (transfers []TokenTransfe
 	if err != nil {
 		return
 	}
-	return getTransfers(rows)
+	return getNamedTransfers(rows)
 }
 
 // MaxBlock for a specified token
--- transferChannels.go
+++ transferChannels.go
@@ -30,3 +30,32 @@ func (ch *TransferChannel) ReadTokenTransfers() (err error) {
 	}
 	return
 }
+
+// NamedTransferChannel is a channel returning all the latest transfers
+type NamedTransferChannel struct {
+	ID    uint64
+	Ch    chan NamedTokenTransfer
+	Error error
+}
+
+// MakeTokenTransferChannel returns a channel to token transfers
+func MakeNamedTokenTransferChannel(ID uint64) (ch NamedTransferChannel) {
+	ch = NamedTransferChannel{ID: ID, Ch: make(chan NamedTokenTransfer, 50)}
+	return
+}
+
+// ReadTokenTransfers send db to channel
+func (ch *NamedTransferChannel) ReadNamedTokenTransfers(addr string) (err error) {
+	defer close(ch.Ch)
+	tt := NamedTokenTransfer{TokenID: ch.ID}
+	ttRes, err := tt.FindAllByAddress(addr)
+	if err != nil {
+		ch.Error = err
+		return
+	}
+	fmt.Println(len(ttRes), " results found for ",addr)
+	for _, transfer := range ttRes {
+		ch.Ch <- transfer
+	}
+	return
+}
